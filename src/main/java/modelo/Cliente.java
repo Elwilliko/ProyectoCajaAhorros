316de13f8bad3a0c3da523a8fb00d96c5025d98f
Orlando Real <OrlandoR54@users.noreply.github.com>
2021-01-06 22:38:03 -0500

    Cliente

---
+++ src/main/java/modelo/Cliente.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class Cliente extends Persona{
+	private int codigo;
+	private String contraseña;
+	/**
+	 * @return the codigo
+	 */
+	public int getCodigo() {
+		return codigo;
+	}
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	/**
+	 * @return the contraseña
+	 */
+	public String getContraseña() {
+		return contraseña;
+	}
+	/**
+	 * @param contraseña the contraseña to set
+	 */
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return "Cliente [codigo=" + codigo + ", contraseña=" + contraseña + "]";
+	}
+
+}
