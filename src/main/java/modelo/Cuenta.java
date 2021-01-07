eab4a95754d9ad8e4c76fb457689665d7b8f2ad1
Kevin Cordero <46872883+wonejo92@users.noreply.github.com>
2021-01-06 22:09:43 -0500

    Cuenta
    Creacion de la clase Cuenta en  el Modelo

---
+++ src/main/java/modelo/Cuenta.java
@@ -0,0 +1,39 @@
+package modelo;
+
+public class Cuenta {
+    //Declaracion de variables
+    private int cuenta_id;
+    private int cuenta_numero;
+    private double cuenta_saldo;
+
+    //Construcor
+    public Cuenta(){
+
+    }
+
+    //Getters and Setters.
+
+    public int getCuenta_id() {
+        return cuenta_id;
+    }
+
+    public void setCuenta_id(int cuenta_id) {
+        this.cuenta_id = cuenta_id;
+    }
+
+    public int getCuenta_numero() {
+        return cuenta_numero;
+    }
+
+    public void setCuenta_numero(int cuenta_numero) {
+        this.cuenta_numero = cuenta_numero;
+    }
+
+    public double getCuenta_saldo() {
+        return cuenta_saldo;
+    }
+
+    public void setCuenta_saldo(double cuenta_saldo) {
+        this.cuenta_saldo = cuenta_saldo;
+    }
+}
